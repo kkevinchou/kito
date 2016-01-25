398f76d729838f6a69f6c96d2ee4a044d73898db
Kevin Chou <kchou@twitch.tv>
2016-01-25 00:09:47 -0800

    cleanup

--- assets/assets.go
+++ assets/assets.go
@@ -1,18 +1,9 @@
 package assets
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-
 	"path/filepath"
 
 	"github.com/veandco/go-sdl2/sdl"
-	"github.com/veandco/go-sdl2/sdl_image"
 	"github.com/veandco/go-sdl2/sdl_ttf"
 )
 
@@ -20,12 +11,6 @@ type Manager struct {
 	icons      map[string]*sdl.Texture
 	fonts      map[string]*ttf.Font
 	animations map[string]*AnimationDefinition
-}
-
-type AnimationMetaData struct {
-	Fps       int `json:"fps"`
-	NumFrames int `json:"num_frames"`
-	Name      string
 }
 
 func NewAssetManager(renderer *sdl.Renderer, directory string) *Manager {
@@ -50,125 +35,4 @@ func (assetManager *Manager) GetFont(filename string) *ttf.Font {
 
 func (assetManager *Manager) GetAnimation(animation string) *AnimationDefinition {
 	return assetManager.animations[animation]
-}
-
-func loadAnimations(directory string, renderer *sdl.Renderer) map[string]*AnimationDefinition {
-	files, err := ioutil.ReadDir(directory)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	animations := map[string]*AnimationDefinition{}
-
-	for _, file := range files {
-		if !file.IsDir() {
-			continue
-		}
-
-		animations[file.Name()] = loadAnimation(filepath.Join(directory, file.Name()), renderer)
-	}
-
-	return animations
-}
-
-func loadAnimation(directory string, renderer *sdl.Renderer) *AnimationDefinition {
-	metaDataFile, err := os.Open(filepath.Join(directory, "meta.json"))
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	bytes, err := ioutil.ReadAll(metaDataFile)
-	var metaData AnimationMetaData
-	json.Unmarshal(bytes, &metaData)
-
-	metaData.Name = filepath.Base(directory)
-
-	files, err := ioutil.ReadDir(directory)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	frames := make([]*sdl.Texture, metaData.NumFrames)
-
-	for _, file := range files {
-		if filepath.Ext(file.Name()) != ".png" {
-			continue
-		}
-
-		framePath := filepath.Join(directory, file.Name())
-		texture, err := img.LoadTexture(renderer, framePath)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-
-		frameIndex, err := strconv.Atoi(strings.Split(file.Name(), ".")[0])
-		frames[frameIndex] = texture
-	}
-
-	a := AnimationDefinition{
-		frames:    frames,
-		numFrames: metaData.NumFrames,
-		fps:       metaData.Fps,
-	}
-
-	return &a
-}
-
-func loadFonts(directory string) map[string]*ttf.Font {
-	fonts := make(map[string]*ttf.Font)
-
-	files, err := ioutil.ReadDir(directory)
-
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	for _, file := range files {
-		fontPath := filepath.Join(directory, file.Name())
-
-		font, err := ttf.OpenFont(fontPath, 15)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		fonts[file.Name()] = font
-	}
-
-	return fonts
-}
-
-func loadTextures(directory string, renderer *sdl.Renderer) map[string]*sdl.Texture {
-	m := make(map[string]*sdl.Texture)
-
-	files, err := ioutil.ReadDir(directory)
-
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	for _, file := range files {
-		imagePath := filepath.Join(directory, file.Name())
-
-		image, err := img.Load(imagePath)
-		if err != nil {
-			fmt.Println("Failed to load \"%s\": %s", imagePath, err)
-			continue
-		}
-
-		texture, err := renderer.CreateTextureFromSurface(image)
-		if err != nil {
-			fmt.Println("Failed to create texture \"%s\": %s", imagePath, err)
-			continue
-		}
-
-		m[file.Name()] = texture
-	}
-
-	return m
 }
---
+++ assets/textures.go
@@ -0,0 +1,41 @@
+package assets
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/sdl_image"
+)
+
+func loadTextures(directory string, renderer *sdl.Renderer) map[string]*sdl.Texture {
+	m := make(map[string]*sdl.Texture)
+
+	files, err := ioutil.ReadDir(directory)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	for _, file := range files {
+		imagePath := filepath.Join(directory, file.Name())
+
+		image, err := img.Load(imagePath)
+		if err != nil {
+			fmt.Println("Failed to load \"%s\": %s", imagePath, err)
+			continue
+		}
+
+		texture, err := renderer.CreateTextureFromSurface(image)
+		if err != nil {
+			fmt.Println("Failed to create texture \"%s\": %s", imagePath, err)
+			continue
+		}
+
+		m[file.Name()] = texture
+	}
+
+	return m
+}
