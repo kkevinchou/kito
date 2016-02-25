d3a4407829e90a98aeec6776bca8a9147b5be24a
Kevin Chou <kchou@twitch.tv>
2016-02-24 22:52:35 -0800

    inventory component

--- components/inventory/inventory.go
+++ components/inventory/inventory.go
@@ -6,10 +6,19 @@ type InventoryI interface {
 	HasItem(interface{})
 }
 
+type NilItem struct {
+}
+
+func (n *NilItem) Id() int {
+	return -1
+}
+
 type ItemI interface {
+	Id() int
 }
 
 type InventoryComponent struct {
+	items map[int]ItemI
 }
 
 func NewInventoryComponent() *InventoryComponent {
@@ -18,5 +27,14 @@ func NewInventoryComponent() *InventoryComponent {
 }
 
 func (i *InventoryComponent) Give(item ItemI) {
+	i.items[item.Id()] = item
+}
 
+func (i *InventoryComponent) Take(id int) (ItemI, bool) {
+	if item, ok := i.items[id]; ok {
+		delete(i.items, id)
+		return item, true
+	}
+
+	return &NilItem{}, false
 }
