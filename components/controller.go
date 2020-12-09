cb9f8a1afd485c95d4a15e4a6b1c679ada1e122b
kkevinchou <>
2020-12-09 01:48:54 -0800

    adding files

---
+++ components/controller.go
@@ -0,0 +1,18 @@
+package components
+
+type ControllerComponent struct {
+	controlled bool
+}
+
+func NewControllerComponent() *ControllerComponent {
+	component := ControllerComponent{}
+	return &component
+}
+
+func (c *ControllerComponent) SetControlled(controlled bool) {
+	c.controlled = controlled
+}
+
+func (c *ControllerComponent) Controlled() bool {
+	return c.controlled
+}
