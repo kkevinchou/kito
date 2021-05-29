939621997721775e86aa3b0e7eb40e386ad62d27
kkevinchou <>
2021-05-29 02:34:50 -0700

    antialiasing shadows with percentage-closer filtering

--- shaders/basicshadow.fs
+++ shaders/basicshadow.fs
@@ -27,7 +27,20 @@ float ShadowCalculation(vec4 fragPosLightSpace, vec3 normal, vec3 lightDir)
 
     // bias term needs to be tweaked depending on geometry
     float bias = max(0.01 * (1.0 - dot(normal, lightDir)), 0.001);
-    float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+    
+    // float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+    for(int x = -1; x <= 1; ++x)
+    {
+        for(int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+            shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;        
+        }    
+    }
+    shadow /= 9.0;
 
     return shadow;
 }
