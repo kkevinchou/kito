b1b5dc3328b638c2a9563d9f8e9426429dc5da3c
Kevin <kkevinchou@hotmail.com>
2021-07-20 01:24:39 -0400

    consolidate bob (#30)

--- entities/bob.go
+++ entities/bob.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kkevinchou/kito/directory"
 	"github.com/kkevinchou/kito/lib/animation"
 	"github.com/kkevinchou/kito/types"
+	"github.com/kkevinchou/kito/utils"
 )
 
 func NewBob(position mgl64.Vec3) *EntityImpl {
@@ -27,7 +28,13 @@ func NewBob(position mgl64.Vec3) *EntityImpl {
 	}
 
 	modelSpec := assetManager.GetAnimatedModel("bob")
-	animatedModel := animation.NewAnimatedModel(modelSpec, 50, 3)
+
+	var animatedModel *animation.AnimatedModel
+	if utils.IsClient() {
+		animatedModel = animation.NewAnimatedModel(modelSpec, 50, 3)
+	} else {
+		animatedModel = animation.NewAnimatedModelWithoutMesh(modelSpec, 50, 3)
+	}
 
 	animationComponent := &components.AnimationComponent{
 		AnimatedModel: animatedModel, // potentially shared across many entities
@@ -42,16 +49,21 @@ func NewBob(position mgl64.Vec3) *EntityImpl {
 		Controlled: true,
 	}
 
+	entityComponents := []components.Component{
+		&components.NetworkComponent{},
+		transformComponent,
+		animationComponent,
+		physicsComponent,
+		thirdPersonControllerComponent,
+	}
+
+	if utils.IsClient() {
+		entityComponents = append(entityComponents, renderComponent)
+	}
+
 	entity := NewEntity(
 		"bob",
-		components.NewComponentContainer(
-			&components.NetworkComponent{},
-			transformComponent,
-			renderComponent,
-			animationComponent,
-			physicsComponent,
-			thirdPersonControllerComponent,
-		),
+		components.NewComponentContainer(entityComponents...),
 	)
 
 	return entity
