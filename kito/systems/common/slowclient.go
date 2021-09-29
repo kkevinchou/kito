147b81f5438368f037374cf260e360546e4fe0b5
kkevinchou <kkevinchou@hotmail.com>
2021-09-28 23:54:02 -0400

    slow client

---
+++ kito/systems/common/slowclient.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kkevinchou/kito/lib/network"
+)
+
+type ArtificallySlowClient struct {
+	client  *network.Client
+	latency time.Duration
+}
+
+func NewArtificallySlowClient(client *network.Client, latency time.Duration) *ArtificallySlowClient {
+	return &ArtificallySlowClient{client: client, latency: latency}
+}
+
+func (c *ArtificallySlowClient) SendMessage(messageType network.MessageType, subMessage interface{}) error {
+	go func() {
+		time.Sleep(c.latency)
+		err := c.client.SendMessage(messageType, subMessage)
+		if err != nil {
+			fmt.Println("artificiallySlowClient send message failed with error", err)
+		}
+	}()
+	return nil
+}
+
+func (c *ArtificallySlowClient) PullIncomingMessages() []*network.Message {
+	return c.client.PullIncomingMessages()
+}
