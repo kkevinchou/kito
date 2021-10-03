832dcd97631d1ba4ff49d88cd14a634ec4c6fefa
kkevinchou <kkevinchou@hotmail.com>
2021-10-03 03:11:59 -0400

    extra logging detail

--- kito/systems/networkdispatch/clientmessagehandler.go
+++ kito/systems/networkdispatch/clientmessagehandler.go
@@ -88,7 +88,8 @@ func validateClientPrediction(gameStateUpdate *knetwork.GameStateUpdateMessage, 
 		historyEntity := cf.PostCFState
 		if historyEntity.Position != entitySnapshot.Position || historyEntity.Orientation != entitySnapshot.Orientation {
 			fmt.Printf(
-				"--------------------------------------\nCLIENT-SIDE\nPREDICTION MISS\nlastCF: %d\nlastGlobalCF: %d\ncurrentGlobalCF: %d\n%v\n%v\n",
+				"--------------------------------------\n[CF:%d]CLIENT-SIDE PREDICTION MISS\nlastCF: %d\nlastGlobalCF: %d\ncurrentGlobalCF: %d\n%v\n%v\n",
+				world.CommandFrame(),
 				gameStateUpdate.LastInputCommandFrame,
 				gameStateUpdate.LastInputGlobalCommandFrame,
 				gameStateUpdate.CurrentGlobalCommandFrame,
