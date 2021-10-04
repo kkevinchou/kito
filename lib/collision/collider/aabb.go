aed3d0c09f7de2b944381ff5079beef59aee309e
kkevinchou <kkevinchou@hotmail.com>
2021-10-03 21:56:34 -0400

    COMMIT START

---
+++ lib/collision/collider/aabb.go
@@ -0,0 +1,10 @@
+package collider
+
+import "github.com/kkevinchou/kito/lib/model"
+
+type BoundingBox struct {
+}
+
+func CreateBoundingBoxFromMesh(mesh *model.Mesh) BoundingBox {
+	return BoundingBox{}
+}
---
+++ lib/collision/collider/capsule.go
@@ -0,0 +1,10 @@
+package collider
+
+import "github.com/kkevinchou/kito/lib/model"
+
+type Capsule struct {
+}
+
+func CreateCapsuleFromMesh(mesh *model.Mesh) Capsule {
+	return Capsule{}
+}
