58a5f05fb963a4ed689eeddf745918ab93b67361
kkevinchou <kkevinchou@hotmail.com>
2022-06-28 04:05:54 -0400

    more collision refactoring

--- kito/systems/collision/collision.go
+++ kito/systems/collision/collision.go
@@ -52,44 +52,52 @@ func (s *CollisionSystem) Update(delta time.Duration) {
 
 	if utils.IsClient() {
 		player := s.world.GetPlayerEntity()
-		s.collide(player, handledCollisions)
+		for _, e2 := range s.world.QueryEntity(components.ComponentFlagCollider | components.ComponentFlagTransform) {
+			if handledCollisions[player.GetID()][e2.GetID()] || handledCollisions[e2.GetID()][player.GetID()] {
+				continue
+			}
+			s.collide(player, e2, handledCollisions)
+			handledCollisions[player.GetID()][e2.GetID()] = true
+			handledCollisions[e2.GetID()][player.GetID()] = true
+		}
 	} else {
 		for _, e1 := range s.world.QueryEntity(components.ComponentFlagCollider | components.ComponentFlagTransform) {
-			s.collide(e1, handledCollisions)
-		}
-	}
-}
-
-func (s *CollisionSystem) collide(e1 entities.Entity, handledCollisions map[int]map[int]bool) {
-	for _, e2 := range s.world.QueryEntity(components.ComponentFlagCollider | components.ComponentFlagTransform) {
-		// don't check an entity against itself or if we've already computed collisions
-		if e1.GetID() == e2.GetID() {
-			continue
-		}
-		if handledCollisions[e1.GetID()][e2.GetID()] || handledCollisions[e2.GetID()][e1.GetID()] {
-			continue
-		}
-
-		isSupportedCollision, capsuleEntity, triMeshEntity := isCapsulteTriMeshCollision(e1, e2)
-		if isSupportedCollision {
-			contactManifolds := collision.CheckCollisionCapsuleTriMesh(*capsuleEntity.GetComponentContainer().ColliderComponent.TransformedCapsuleCollider, *triMeshEntity.GetComponentContainer().ColliderComponent.TransformedTriMeshCollider)
-			if contactManifolds != nil {
-				capsuleEntity.GetComponentContainer().ColliderComponent.CollisionInstances = append(
-					capsuleEntity.GetComponentContainer().ColliderComponent.CollisionInstances,
-					&components.CollisionInstance{
-						OtherEntityID:    capsuleEntity.GetID(),
-						ContactManifolds: contactManifolds,
-					},
-				)
+			for _, e2 := range s.world.QueryEntity(components.ComponentFlagCollider | components.ComponentFlagTransform) {
+				if handledCollisions[e1.GetID()][e2.GetID()] || handledCollisions[e2.GetID()][e1.GetID()] {
+					continue
+				}
+				s.collide(e1, e2, handledCollisions)
+				handledCollisions[e1.GetID()][e2.GetID()] = true
+				handledCollisions[e2.GetID()][e1.GetID()] = true
 			}
-			// TODO: decide how we want to handle avoiding double calculating collisions
-			handledCollisions[capsuleEntity.GetID()][triMeshEntity.GetID()] = true
-			handledCollisions[triMeshEntity.GetID()][capsuleEntity.GetID()] = true
 		}
 	}
 }
 
-func isCapsulteTriMeshCollision(e1, e2 entities.Entity) (bool, entities.Entity, entities.Entity) {
+func (s *CollisionSystem) collide(e1 entities.Entity, e2 entities.Entity, handledCollisions map[int]map[int]bool) {
+	// don't check an entity against itself or if we've already computed collisions
+	if e1.GetID() == e2.GetID() {
+		return
+	}
+
+	isSupportedCollision, capsuleEntity, triMeshEntity := isCapsuleTriMeshCollision(e1, e2)
+	if !isSupportedCollision {
+		return
+	}
+
+	contactManifolds := collision.CheckCollisionCapsuleTriMesh(*capsuleEntity.GetComponentContainer().ColliderComponent.TransformedCapsuleCollider, *triMeshEntity.GetComponentContainer().ColliderComponent.TransformedTriMeshCollider)
+	if contactManifolds != nil {
+		capsuleEntity.GetComponentContainer().ColliderComponent.CollisionInstances = append(
+			capsuleEntity.GetComponentContainer().ColliderComponent.CollisionInstances,
+			&components.CollisionInstance{
+				OtherEntityID:    capsuleEntity.GetID(),
+				ContactManifolds: contactManifolds,
+			},
+		)
+	}
+}
+
+func isCapsuleTriMeshCollision(e1, e2 entities.Entity) (bool, entities.Entity, entities.Entity) {
 	e1cc := e1.GetComponentContainer()
 	e2cc := e2.GetComponentContainer()
 
@@ -99,9 +107,9 @@ func isCapsulteTriMeshCollision(e1, e2 entities.Entity) (bool, entities.Entity, 
 		}
 	}
 
-	if e1cc.ColliderComponent.CapsuleCollider != nil {
-		if e2cc.ColliderComponent.TriMeshCollider != nil {
-			return true, e1, e2
+	if e2cc.ColliderComponent.CapsuleCollider != nil {
+		if e1cc.ColliderComponent.TriMeshCollider != nil {
+			return true, e2, e1
 		}
 	}
 
