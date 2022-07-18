5088601af00ab891b7404b17596b7b85529a44c7
kkevinchou <kkevinchou@hotmail.com>
2022-07-18 01:36:13 -0400

    console files

---
+++ kito/console/console.go
@@ -0,0 +1,17 @@
+package console
+
+var GlobalConsole *Console = &Console{}
+
+type ConsoleItem struct {
+	Command string
+	Output  string
+}
+
+type Console struct {
+	ConsoleItems []*ConsoleItem
+	Input        string
+}
+
+func (c *Console) Send() {
+	c.ConsoleItems = append(c.ConsoleItems, &ConsoleItem{Command: c.Input})
+}
