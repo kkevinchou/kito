21d2780a8e9ff4d78992658c1af4b7f2b04f89d1
kkevinchou <kkevinchou@hotmail.com>
2022-10-08 00:48:03 -0400

    cleanup preframe

--- kito/systems/preframe/preframe.go
+++ kito/systems/preframe/preframe.go
@@ -3,12 +3,9 @@ package preframe
 import (
 	"time"
 
-	"github.com/kkevinchou/kito/kito/components"
 	"github.com/kkevinchou/kito/kito/entities"
 	"github.com/kkevinchou/kito/kito/singleton"
 	"github.com/kkevinchou/kito/kito/systems/base"
-	"github.com/kkevinchou/kito/kito/utils"
-	"github.com/kkevinchou/kito/lib/input"
 )
 
 type World interface {
@@ -29,17 +26,6 @@ func NewPreFrameSystem(world World) *PreFrameSystem {
 }
 
 func (s *PreFrameSystem) Update(delta time.Duration) {
-	if utils.IsServer() {
-		singleton := s.world.GetSingleton()
-		for i, _ := range singleton.PlayerInput {
-			singleton.PlayerInput[i] = input.Input{}
-		}
-	}
-
-	// reset notepad
-	for _, entity := range s.world.QueryEntity(components.ComponentFlagNotepad) {
-		entity.GetComponentContainer().NotepadComponent.LastAction = components.ActionNone
-	}
 
 }
 
