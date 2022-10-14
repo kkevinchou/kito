1154bc32513f8e1ddf66b81856646c51fe10f6d4
kkevinchou <kkevinchou@hotmail.com>
2022-10-13 22:29:41 -0400

    remove QuatLookAt tests

--- lib/libutils/math_test.go
+++ lib/libutils/math_test.go
@@ -4,31 +4,7 @@ import (
 	"testing"
 
 	"github.com/go-gl/mathgl/mgl64"
-	"github.com/kkevinchou/kito/lib/libutils"
 )
-
-func TestLookAt(t *testing.T) {
-	eye := mgl64.Vec3{0, 0, 0}
-	up := mgl64.Vec3{0, 1, 0}
-
-	dir := mgl64.Vec3{0, 1, 0}
-	libutils.QuatLookAt(eye, dir, up)
-
-	dir = mgl64.Vec3{0, 0, -1}
-	libutils.QuatLookAt(eye, dir, up)
-
-	dir = mgl64.Vec3{-1, 0, 0}
-	libutils.QuatLookAt(eye, dir, up)
-
-	dir = mgl64.Vec3{1, 0, 0}
-	libutils.QuatLookAt(eye, dir, up)
-
-	dir = mgl64.Vec3{0, -1, 0}
-	libutils.QuatLookAt(eye, dir, up)
-
-	dir = mgl64.Vec3{0, 0, 1}
-	libutils.QuatLookAt(eye, dir, up)
-}
 
 func TestQuat(t *testing.T) {
 	v1 := mgl64.Vec3{0, 0, -1}
